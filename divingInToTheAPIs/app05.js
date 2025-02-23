234a00bbaf839c895920ff74996979fe5e6a96f9
Ravinamohite07 <ravinamohite527@gmail.com>
2025-02-23 23:26:03 +0530

    user updated using findByIdAndUpdate

---
+++ divingInToTheAPIs/app05.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const connectDB = require("./config/database");
+const app = express();
+const User = require("./models/users");
+
+app.use(express.json())
+
+app.patch("/user",async (req,res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+    try{
+        await User.findByIdAndUpdate({_id: userId},data);
+        res.send("user updated successfully...!");
+    } catch (err) {
+
+        res.status(400).send("Something went wrong...!");
+
+    }
+})
+
+
+
+
+connectDB()
+  .then(() =>{
+     console.log("Database connection established...!");
+     app.listen(3000,() => {
+        console.log("Server successfully run on port 3000..!");
+    })
+  }).catch((err) => {
+     console.log("Database cannot connection established..!!");
+  });
+
